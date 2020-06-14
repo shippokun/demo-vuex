799fbb7d63ab99d33ba9db67615a5d4d932f82f9
shippokun <okashi0tabetai@gmail.com>
2020-06-15 02:16:43 +0900

    feat: init setting

---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  entry: "./src/main.js",
+  resolve: {
+    extensions: [".ts"]
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: [
+          {
+            loader: "ts-loader",
+            options: {
+              transpileOnly: true // 型チェックしない！！！
+            }
+          }
+        ]
+      }
+    ]
+  }
+};
