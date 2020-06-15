a4a7c886d370bf988d07657fa712d0ecacc9d9b0
shippokun <okashi0tabetai@gmail.com>
2020-06-16 00:41:50 +0900

    fix: fix typo

--- src/pages/home/components/home-component.spec.js
+++ src/pages/home/components/home-component.spec.js
@@ -2,7 +2,7 @@ import { shallowMount } from "@vue/test-utils";
 import HomeComponent from "./home-component";
 
 describe("home-component.vue", () => {
-  it("renders props.msg when passed", () => {
+  it("renders props.name when passed", () => {
     const name = "test name";
     const wrapper = shallowMount(HomeComponent, { propsData: { name } });
     expect(wrapper.text()).toMatch(name);
